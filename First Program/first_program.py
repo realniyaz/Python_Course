2196563206d3dc388d2e6e9ca1d6ebace712a252
Niyaz Ahmed <real.niyaz2023@gmail.com>
2024-11-07 16:47:58 +0530

    Create first_program.py
    In this Program we understood the ways of printing the statements on to the console.
    
    We used variables and f-string concepts to gain greater knowledge and types by which we can efficiently use the print command in the Python

---
+++ First Program/first_program.py
@@ -0,0 +1,15 @@
+print("Hello, World")
+
+#Print statements using variables
+name = "Niyaz"
+age = 21
+
+print(name, age)
+print("Name:", name, "Age:", age)
+
+#Print statements using f-string concepts
+Name = "Alex"
+Age = 18
+
+print(f"My name is {name} and I am {age} years old")
+print(f"My name is {Name} and I am {Age} years old")
