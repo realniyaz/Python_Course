f7b810163cab868784692e698f95d902e4fdf9e0
Niyaz Ahmed <real.niyaz2023@gmail.com>
2024-11-07 17:18:32 +0530

    Create end_sep_python.py

---
+++ First Program/end_sep_python.py
@@ -0,0 +1,8 @@
+#end: used to add objects at the end of the displayed values
+
+print("Hi","Hello", "Ola", end="Niyaz\n") # We saw Niyaz added at the last of the displayed result
+
+#sep: used to seperate the multiple objects in the print statement
+
+print("Hi", 7, 9, "Niyaz", sep=",")
+print(14 , 6, 2004, sep="-")
