8178985f17489ef98bace347e1445f28819efbfc
Niyaz Ahmed <real.niyaz2023@gmail.com>
2024-11-08 12:08:05 +0530

    Create typecasting.py

---
+++ Typecasting/typecasting.py
@@ -0,0 +1,13 @@
+#Typecasting
+
+s = "20"
+num = 7
+
+sum1 = int(s) + num #explicit typecastimg
+print(f"the sum of two numbers is {sum1}")
+
+i = 8.09
+j = 10
+
+sum2 = i + j #implicit typecasting
+print(sum2)
