3d017a89e043e4e8c6c8cf78b1134f1051b006e2
Niyaz Ahmed <real.niyaz2023@gmail.com>
2024-11-08 10:27:32 +0530

    Create var_datatypes.py

---
+++ Variables & Datatypes/var_datatypes.py
@@ -0,0 +1,14 @@
+#Variables
+
+name = "Niyaz" #string
+age = 21 #numeric 
+b = True #boolean
+nul = None #Null
+
+print(name , age , b, nul, sep=",")
+
+#Datatypes
+print(type(name))
+print(type(age))
+print(type(b))
+print(type(nul))
