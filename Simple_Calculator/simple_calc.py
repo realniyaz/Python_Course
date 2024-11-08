e45854dbea6cf04b19c92fafab4c5d113f8215d5
Niyaz Ahmed <real.niyaz2023@gmail.com>
2024-11-08 10:49:00 +0530

    Create simple_calc.py

---
+++ Simple_Calculator/simple_calc.py
@@ -0,0 +1,19 @@
+""""
+Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. 
+Your program should format the output in a readable manner!
+"""
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
+print(f"The  difference of {num1} and {num2} is {num1 - num2}")
+print(f"The product of {num1} and {num2} is {num1 * num2}")
+print(f"The division of {num1} and {num2} is {num1 / num2}")
+print(f"The remainder of {num1} and {num2} is {num1 % num2}")
