bf75d397d629c1146014069389ba0dd90d98f1e0
Niyaz Ahmed <real.niyaz2023@gmail.com>
2024-11-11 18:05:29 +0530

    Create if_elif_else.py

---
+++ Conditional Statements/if_elif_else.py
@@ -0,0 +1,25 @@
+age = int(input("Enter your age: "))
+if (age>18):
+    print("Eligible to Cast vote")
+
+# if - else statements
+marks = int(input("Enter your total marks out of 500: "))
+if(marks>250):
+    print("Äbove 50 %")
+else:
+    print("Below 50%")
+
+# if-elif-else
+letter = "A"
+
+if letter == "B":
+    print("letter is B")
+
+elif letter == "C":
+    print("letter is C")
+
+elif letter == "A":
+    print("letter is A")
+
+else:
+    print("letter isn't A, B or C")
